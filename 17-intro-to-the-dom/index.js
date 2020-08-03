3401eb952320889172ae1338248a32c6abb7a1f5
Steven Doran <steven.doran@flatironschool.com>
2020-08-03 15:46:43 -0400

    post lecture

---
+++ 17-intro-to-the-dom/index.js
@@ -0,0 +1,15 @@
+console.log("DOM Manipulation is so cool.")
+
+/*
+What is the DOM?
+
+- contains a bunch of objects
+- allows us to edit those objects on-the-fly
+- the object represent the HTML
+- represents the HTML in a data structure that allows us to interact with the HTML as objects
+  - this means we can add new ones, remove them, change them
+- DOM stands for Document Object Model
+
+*/
+
+
