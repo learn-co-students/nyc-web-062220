2c0e9df7956c076a8576188fa17d047b292e9ca4
Steven Doran <steven.doran@flatironschool.com>
2020-08-03 13:36:26 -0400

    post lecture

--- 16-intro-to-js/src/index.js
+++ 16-intro-to-js/src/index.js
@@ -1,16 +1,20 @@
 console.log("Welcome to Mod 3")
+// console.dir => looking at objects
+// console.table => breaks down object properties in a table view
+
 // alert("hey there")
 
 /*
  **Questions
-
+  - node?
 
  **Why JS?
 
   - three pillars - Coffee Dad Twitter
-  - 1. DOM Manipulation 
-  - 2. Event handling 
-  - 3. Fetch 
+  - 1. DOM Manipulation - managing the user interaction with the website, what the user sees etc.
+  - 2. Event handling - introduce interactivity into our browser, capture user input in a richer way
+  - 3. Fetch - communicating with servers or APIs
+
 
   - Single Page Applications 
 
@@ -24,17 +28,16 @@ console.log("Welcome to Mod 3")
  **How JS?
 
   - how to load JS into an HTML document
+  - script tags for local and remote js files
+  - can do inline scripts inside the HTML but probably shouldn't
 
-
-** Debugger and console.log
-
-
-
-** Type checking
-
-
+  - debugger and console.log
 
   
+
+  - type checking
+
+
  **Some data types
  - null
 
@@ -69,4 +72,38 @@ console.log("Welcome to Mod 3")
 
 
 */
+
+// let name = "Steven"
+
+// function something(){
+//   console.log('before')
+//   console.log(name)
+//   // debugger
+//   console.log(name)
+//   console.log('after')
+// }
+
+// something()
+
+// in ruby...
+// def say_hi(name)
+//   puts "Hi there #{name}"
+// end
+
+
+// function declaration
+function sayHi(name) {
+  console.log(`Hey there ${name}`)
+}
+
+// function expression
+let sayHello = function(name){
+  console.log(`Hey there ${name}`)
+}
+
+// arrow syntax
+let sayHowdy = (name) => {
+  console.log(`Howdy ${name}`)
+}
+
 
