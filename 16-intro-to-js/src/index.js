5969912de6e550d8a4ae34870689ee24d6eb71dc
Steven Doran <steven.doran@flatironschool.com>
2020-08-03 10:52:29 -0400

    pre lecture

---
+++ 16-intro-to-js/src/index.js
@@ -0,0 +1,72 @@
+console.log("Welcome to Mod 3")
+// alert("hey there")
+
+/*
+ **Questions
+
+
+ **Why JS?
+
+  - three pillars - Coffee Dad Twitter
+  - 1. DOM Manipulation 
+  - 2. Event handling 
+  - 3. Fetch 
+
+  - Single Page Applications 
+
+
+ 
+ **What is JS?
+
+ - ECMAScript
+
+
+ **How JS?
+
+  - how to load JS into an HTML document
+
+
+** Debugger and console.log
+
+
+
+** Type checking
+
+
+
+  
+ **Some data types
+ - null
+
+ 
+ - Number
+  - only the one Number class
+  - 0 is falsey
+
+ 
+ - String
+  - can use "" or ''
+  - template literal ``
+
+
+
+ - Object Literals
+
+
+ 
+ - Array
+
+
+
+ - undefined
+
+
+
+ **Functions
+  - multiple ways of declaring functions
+  - definition vs invocation 
+
+
+
+*/
+
