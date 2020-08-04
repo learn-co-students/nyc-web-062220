db1ab827f2e58baf91cc5e177c16f3204318c29d
Steven Doran <steven.doran@flatironschool.com>
2020-08-04 12:36:12 -0400

    post lecture

--- 18-intro-to-events/listeners.js
+++ 18-intro-to-events/listeners.js
@@ -2,10 +2,22 @@ console.log("listening")
 
 // As a user, when I click on the welcome image I want to see a turquoise border around that image
 
+// 1. get the node that we want to attach the event listener to
+// 2. add a click listener to the node
+// 3. on click, we want to change the border style of the image
+
+const welcome = document.querySelector("#welcome-image")
+
+// the event object is an object that capture a whole lot of information about the event that just occurred
+welcome.addEventListener('click', function(e){
+  const image = e.target
+  console.dir(image)
+  image.style.border = "solid thick turquoise"
+})
 
 
 
-// when the mouse moves over the welcome image I want to see a picture of a baby sloth, and i want the picture to change back when the mouse moves off the image
+// As a user, when the mouse moves over the welcome image I want to see a picture of a baby sloth, and i want the picture to change back when the mouse moves off the image
 
 
 
